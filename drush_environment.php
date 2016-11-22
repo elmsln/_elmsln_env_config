299bb4887658cef1d7c3f1b0bb26a35944918937
Bryan Ollendyke <btopro@psu.edu>
2016-11-22 13:40:29 -0500

    working example to connect to multiple environments from drush based on environment changes

--- drush_environment.php
+++ drush_environment.php
@@ -1,22 +1,25 @@
 <?php
+// use this in order to define multiple environments to generate new drush alias targets
+$denvironment = array();
+
 // here's an example of what you could do if you wanted to define multiple alternate targets
 // which could be used for something like drush @courses.sing100 sql-sync @prod.courses.sing100 --y
 // this command could be used to sync something from production down to the current environment
 // for testing / automated migration / QA scripts.
-$denvironment = array(
+/*$denvironment = array(
   'qa' => array(
-    'ssh' => '10.0.0.1',
-    'user' => 'vagrant',
-    'port' => '22',
-    'uri' => 'elmsln.local',
+    'remote-host' => '10.0.0.1',
+    'remote-user' => 'jenkins',
+    'ssh-options' => '-p 22',
+    '_base_url' => 'qa.elmsln.local',
   ),
   'prod' => array(
-    'ssh' => '110.110.110.110',
-    'user' => 'jenkins',
-    'port' => '22',
-    'uri' => 'prod.address.local',
+    'remote-host' => '10.0.0.2',
+    'remote-user' => 'jenkins',
+    'ssh-options' => '-p 22',
+    '_base_url' => 'elmsln.local',
   ),
-);
+);*/
 if (empty($denvironment)) {
   $aliases = array();
 }
@@ -25,14 +28,16 @@ else {
   foreach ($aliases as $alias => $aliasvals) {
     // loop through environments
     foreach ($denvironment as $env => $envvals) {
-     	// add in the environments
-      $aliasvals['ssh'] = $envvals['ssh'];
-      $aliasvals['user'] = $envvals['user'];
-      $aliasvals['port'] = $envvals['port'];
+      // add in the environments
+      if (!isset($aliasvals['remote-host'])) {
+        $aliasvals['ssh-options'] = $envvals['ssh-options'];
+        $aliasvals['remote-user'] = $envvals['remote-user'];
+        $aliasvals['remote-host'] = $envvals['remote-host'];
+      }
       // replace the part of the uri address that matches what it was and puts in what it should be
-      $aliasvals['uri'] = str_replace($elmslncfg['address'], $envvals['uri'], $aliasvals['uri']);
+      $aliasvals['uri'] = str_replace($elmslncfg['address'], $envvals['_base_url'], $aliasvals['uri']);
       $aliases[$env . '.' . $alias] = $aliasvals;
+      unset($aliases[$alias]);
     }
-    unset($aliases[$alias]);
   }
 }
